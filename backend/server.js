10cfa850126f9f79164b588c773231183d0f21bf
sudip acharya <sudipachary79@outlook.com>
2024-11-09 17:57:19 +0545

    minor change

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,13 @@ const PORT = process.env.PORT;
 
 import cors from 'cors';
 
+const allowedOrigin = "https://diabetes-tracker-black.vercel.app";
 
 app.use(cors({
-  }));
-
+  origin: allowedOrigin,  // Allow requests only from your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allow specific HTTP methods
+  credentials: true,  // Allow cookies and credentials to be sent
+}));
 
 connectDb();
 app.use(express.json());
