32c8a87786c47850894685d9a58bde2cb832cd05
sudip acharya <sudipachary79@outlook.com>
2024-11-09 20:33:05 +0545

    prod phae 2

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,28 @@
-import express from 'express';
-import 'dotenv/config';
-import register from './routes/userRoute.js';
-import connectDb from './config/dbConnection.js';
+import express from "express"
+import 'dotenv/config'
+import register from "./routes/userRoute.js"
+import connectDb from "./config/dbConnection.js"
+const app = express()
+
+const PORT = process.env.PORT;
+
 import cors from 'cors';
 
-const app = express();
 
-const allowedOrigin = "https://diabetes-tracker-black.vercel.app/";
+app.use(cors({ origin: `https://diabities-tracker-black.vercel.app/`}));
 
-app.use(cors({
-  origin: allowedOrigin,  // Allow requests only from your frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allow specific HTTP methods
-  credentials: true,  // Allow cookies and credentials to be sent
-}));
 
 connectDb();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+app.get("/",(req,res)=>{
+    res.send("Hello world")
 
-app.use('/user', register);
+})
 
-// Export Express app as a serverless function for Vercel
-export default app;
+app.use('/user', register)
+
+app.listen(PORT,()=>{
+    console.log(`Running over the${PORT}`);
+    
+})
